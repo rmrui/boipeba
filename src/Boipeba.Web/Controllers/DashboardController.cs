0acc4597dbf51e977008a4898cabc95a846574f0
ricardoborges <ricardoborges@gmail.com>
2018-06-21 00:11:21 -0300

    base user and dashboard

--- src/Boipeba.Web/Controllers/DashboardController.cs
+++ src/Boipeba.Web/Controllers/DashboardController.cs
@@ -1,12 +1,46 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Boipeba.Core.Modulos.Processos;
+using Boipeba.Core.Modulos.Processos.Repositories;
+using Boipeba.Core.Modulos.Processos.Services;
 
 namespace Boipeba.Web.Controllers
 {
     public class DashboardController: BaseController
     {
+        private readonly IProcessoRepository _processoRepository;
+
+        public DashboardController(IProcessoRepository processoRepository)
+        {
+            _processoRepository = processoRepository;
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetViewData()
+        {
+            //var list = _processoRepository.AtribuidosPara(Usuario.Matricula);
+
+            var list = new List<Processo>
+            {
+                new Processo
+                {
+                    Assunto = new Assunto {DsAssunto = "teste1"},
+                    Simp = "123",
+                    PessoaDestino = new Pessoa {Nome = "Rui"}
+                },
+                new Processo
+                {
+                    Assunto = new Assunto {DsAssunto = "teste2"},
+                    Simp = "456",
+                    PessoaDestino = new Pessoa {Nome = "Rui"}
+                }
+            };
+
+            return Json(new {Processos = list});
         }
     }
 }
--- src/Boipeba.Web/Scripts/app/Dashboard/dashboardCtrl.js
+++ src/Boipeba.Web/Scripts/app/Dashboard/dashboardCtrl.js
@@ -4,7 +4,24 @@
     dashboardCtrl.$inject = ["$scope", "$http", "$window", "toastr"];
 
     function dashboardCtrl($scope, $http, $window, toastr) {
-        $scope.msg = "Rui";
+
+        $scope.view = {}
+        $scope.viewdata = {}
+
+        $scope.setup = function () {
+
+            $scope.view.loadingList = true;
+
+            $http({ method: "POST", url: "/Dashboard/GetViewData" })
+                .then(function success(response) {
+                        $scope.view.loadingList = false;
+                        $scope.viewdata.processos = response.data.Processos;
+                    },
+                    function error(response) {
+                        $scope.view.loadingList = false;
+                    });
+        }
+
     }
 
     angular
